cb66f8dd61db595b1ec05c53583bfb65891fb0ad
Douglas <douglas@esneadd1194817.sp.local>
2023-01-04 10:42:02 -0300

    :bug: new mode history

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from "vue-router"
+import { createRouter, createWebHashHistory } from "vue-router"
 
 import { useUserStore } from '../stores/user'
 
@@ -30,7 +30,7 @@ const routes = [
 ]
 
 const router = createRouter({
-    history: createWebHistory(),
+    history: createWebHashHistory(),
     routes,
     linkActiveClass: 'active-link'
 })
