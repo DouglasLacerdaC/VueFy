4cec4ad143bb52ad98b28b5abc61fe28a548b8b0
Douglas <douglas@esneadd1194817.sp.local>
2023-01-04 10:13:43 -0300

    :bug: resources in production

--- src/data/lists.js
+++ src/data/lists.js
@@ -1,21 +1,21 @@
 export const forYou = [
     {
-        img: '/src/assets/imgs/banners/theeminemshow.jpg',
+        img: '/imgs/banners/theeminemshow.jpg',
         title: 'Eminem',
         description: 'Melhores do Eminem'
     },
     {
-        img: '/src/assets/imgs/banners/copa.png',
+        img: '/imgs/banners/copa.png',
         title: 'Copa 2022',
         description: 'Músicas da Copa'
     },
     {
-        img: '/src/assets/imgs/banners/gatinhodorock.jpg',
+        img: '/imgs/banners/gatinhodorock.jpg',
         title: 'Rock',
         description: 'O melhor do rock'
     },
     {
-        img: '/src/assets/imgs/banners/lol.png',
+        img: '/imgs/banners/lol.png',
         title: 'Podcast Lol',
         description: 'Lol'
     },
--- src/data/playlists.js
+++ src/data/playlists.js
@@ -3,48 +3,48 @@ export const recommendedSongs = [
         name: 'Wuthering Heights',
         band: 'Angra',
         music: {
-            banner: '/src/assets/imgs/banners/angelscry.jpg',
-            url: '/src/assets/audio/WutheringHeights.mp3',
+            banner: '/imgs/banners/angelscry.jpg',
+            url: '/audio/WutheringHeights.mp3',
         }
     },
     {
         name: "Master Of Puppets",
         band: 'Metallica',
         music: {
-            banner: '/src/assets/imgs/banners/masterofpuppets.jpg',
-            url: '/src/assets/audio/MasterOfPuppets.mp3',
+            banner: '/imgs/banners/masterofpuppets.jpg',
+            url: '/audio/MasterOfPuppets.mp3',
         }
     },
     {
         name: 'Smells Like Teen Spirit',
         band: 'Nirvana',
         music: {
-            banner: '/src/assets/imgs/banners/nevermind.jpeg',
-            url: '/src/assets/audio/SmellsLikeTeenSpirit.mp3',
+            banner: '/imgs/banners/nevermind.jpeg',
+            url: '/audio/SmellsLikeTeenSpirit.mp3',
         }
     },
     {
         name: "Sweet Child O'Mine",
         band: "Guns N' Roses",
         music: {
-            banner: '/src/assets/imgs/banners/appetite.jpeg',
-            url: '/src/assets/audio/SweetChildOMine.mp3',
+            banner: '/imgs/banners/appetite.jpeg',
+            url: '/audio/SweetChildOMine.mp3',
         }
     },
     {
         name: 'Fear of the dark',
         band: 'Iron maiden',
         music: {
-            banner: '/src/assets/imgs/banners/fearofthedark.jpg',
-            url: '/src/assets/audio/FearOfTheDark.mp3',
+            banner: '/imgs/banners/fearofthedark.jpg',
+            url: '/audio/FearOfTheDark.mp3',
         }
     },
     {
         name: "Bohemian Rhapsody",
         band: 'Queen',
         music: {
-            banner: '/src/assets/imgs/banners/bohemian.jpg',
-            url: '/src/assets/audio/BohemianRhapsody.mp3',
+            banner: '/imgs/banners/bohemian.jpg',
+            url: '/audio/BohemianRhapsody.mp3',
         }
     },
 ]
@@ -55,24 +55,24 @@ export const randomSongs = [
         name: "Nothing Else Matters",
         band: 'Metallica',
         music: {
-            banner: '/src/assets/imgs/banners/metallica.jpg',
-            url: '/src/assets/audio/NothingElseMatters.mp3',
+            banner: '/imgs/banners/metallica.jpg',
+            url: '/audio/NothingElseMatters.mp3',
         }
     },
     {
         name: "Chop Suey!",
         band: 'System Of A Down',
         music: {
-            banner: '/src/assets/imgs/banners/toxicity.png',
-            url: '/src/assets/audio/ChopSuey.mp3',
+            banner: '/imgs/banners/toxicity.png',
+            url: '/audio/ChopSuey.mp3',
         }
     },
     {
         name: "Without Me",
         band: 'Eminem',
         music: {
-            banner: '/src/assets/imgs/banners/theeminemshow.jpg',
-            url: '/src/assets/audio/WithoutMe.mp3',
+            banner: '/imgs/banners/theeminemshow.jpg',
+            url: '/audio/WithoutMe.mp3',
         }
     },
 ]
